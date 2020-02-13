44e8c17d64a2539f11498c4cc79db4c862548dbd
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2020-02-13 14:30:28 -0400

    work

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,20 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  photo: SafeResourceUrl;
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri
+    });
+    console.log(image);
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.webPath));
+  }
 
 }
